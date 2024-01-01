66929de4f5b3e0456c6a67680bcf618be5daf879
jajarosi <jarodrosinoff@gmail.com>
2024-01-01 17:12:21 +0200

    first commit basis

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // Gestionnaire d'événements pour les liens externes
+    $(".external-link").click(function () {
+        var url = $(this).data("url");
+        window.open(url, '_blank');
+    });
+
+    // Masquer toutes les sections sauf la section "home"
+    $("section:not(#home)").hide();
+
+    // Gestionnaire d'événements pour les liens internes
+    $("a").on("click", function () {
+        const dataSection = $(this).attr("data-section")
+        console.log(dataSection)
+        $("section").hide()
+        $("#" + dataSection).show()
+    });
+});
